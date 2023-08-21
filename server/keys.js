97ce9b5093e14ccb397f5d9817f4f7aeb933923e
Madhwansh <108359887+Madhwansh@users.noreply.github.com>
2023-08-21 19:18:41 +0530

    keys.js

--- server/keys.js
+++ server/keys.js
@@ -1,6 +1,6 @@
-module.exports = {
-  MONGOURI:
-    "mongodb+srv://madhwanshsrivastava:monu862001@cluster0.fvqvbew.mongodb.net/?retryWrites=true&w=majority",
-  JWT_SECRET: "effegwegw2r",
+//module.exports = {
+ // MONGOURI:
+   // "mongodb+srv://madhwanshsrivastava:monu862001@cluster0.fvqvbew.mongodb.net/?retryWrites=true&w=majority",
+ // JWT_SECRET: "effegwegw2r",
 };
 //monu862001
